ffb55b6576181c8e953601c1c3ead744ffde3510
Pavel Savchenko <asfaltboy@gmail.com>
2019-08-18 10:48:44 +0200

    refactor: reuse handler creators in tests

--- handler_test.go
+++ handler_test.go
@@ -8,23 +8,35 @@ import (
 	"testing"
 )
 
-func TestMapHandlerMatched(t *testing.T) {
-	req, err := http.NewRequest("GET", "/urlshort-godoc", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+// Build the fallback default mux
+func getDefaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	})
+	return mux
+}
 
+func getMapHandler(fallback *http.ServeMux) http.Handler {
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 
+	return MapHandler(pathsToUrls, fallback)
+}
+
+func TestMapHandlerMatched(t *testing.T) {
+	req, err := http.NewRequest("GET", "/urlshort-godoc", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler := getMapHandler(nil)
 	rr := httptest.NewRecorder()
-	handler := MapHandler(pathsToUrls, nil)
 
 	handler.ServeHTTP(rr, req)
-
 	res := rr.Result()
 	if status := res.StatusCode; status != http.StatusFound {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusFound)
@@ -43,17 +55,9 @@ func TestMapHandlerNotMatched(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
-	})
-
-	// Build the MapHandler using the mux as the fallback
-	pathsToUrls := map[string]string{}
-
+	mux := getDefaultMux()
+	handler := getMapHandler(mux)
 	rr := httptest.NewRecorder()
-	handler := MapHandler(pathsToUrls, mux)
-
 	handler.ServeHTTP(rr, req)
 
 	res := rr.Result()
@@ -68,26 +72,30 @@ func TestMapHandlerNotMatched(t *testing.T) {
 	}
 }
 
+// Build the JSONHandler using given fallback
+func getYAMLHandler(fallback *http.ServeMux) http.Handler {
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution`
+
+	handler, err := YAMLHandler([]byte(yaml), fallback)
+	if err != nil {
+		panic(err)
+	}
+	return handler
+}
+
 func TestYAMLHandlerMatched(t *testing.T) {
 	req, err := http.NewRequest("GET", "/urlshort", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Build the YAMLHandler using the mux as the fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-
+	// Build the YAMLHandler using given fallback
+	handler := getYAMLHandler(nil)
 	rr := httptest.NewRecorder()
-	handler, err := YAMLHandler([]byte(yaml), nil)
-	if err != nil {
-		panic(err)
-	}
-
 	handler.ServeHTTP(rr, req)
 
 	res := rr.Result()
@@ -109,22 +117,9 @@ func TestYAMLHandlerNotMatched(t *testing.T) {
 	}
 
 	// Build the YAMLHandler using the mux as the fallback
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
-	})
-
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-`
-
+	mux := getDefaultMux()
+	handler := getYAMLHandler(mux)
 	rr := httptest.NewRecorder()
-	handler, err := YAMLHandler([]byte(yaml), mux)
-	if err != nil {
-		panic(err)
-	}
-
 	handler.ServeHTTP(rr, req)
 
 	res := rr.Result()
@@ -148,24 +143,28 @@ func TestInvalidYAMLHandler(t *testing.T) {
 	}
 }
 
+// Build the JSONHandler using given fallback
+func getJSONHandler(fallback *http.ServeMux) http.Handler {
+	json := `[{"path": "/urlshort", "url": "https://github.com/gophercises/urlshort"},
+{"path": "/urlshort-final", "url": "https://github.com/gophercises/urlshort/tree/solution"}]`
+
+	handler, err := JSONHandler([]byte(json), fallback)
+	if err != nil {
+		panic(err)
+	}
+	return handler
+}
+
 func TestJSONHandlerMatched(t *testing.T) {
 	req, err := http.NewRequest("GET", "/urlshort", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Build the JSONHandler using the mux as the fallback
-	json := `[{"path": "/urlshort", "url": "https://github.com/gophercises/urlshort"},
-{"path": "/urlshort-final", "url": "https://github.com/gophercises/urlshort/tree/solution"}]`
+	handler := getYAMLHandler(nil)
 
 	rr := httptest.NewRecorder()
-	handler, err := JSONHandler([]byte(json), nil)
-	if err != nil {
-		panic(err)
-	}
-
 	handler.ServeHTTP(rr, req)
-
 	res := rr.Result()
 	if status := res.StatusCode; status != http.StatusFound {
 		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusFound)
@@ -185,20 +184,10 @@ func TestJSONHandlerNotMatched(t *testing.T) {
 	}
 
 	// Build the JSONHandler using the mux as the fallback
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
-	})
-
-	json := `[{"path": "/urlshort", "url": "https://github.com/gophercises/urlshort"},
-{"path": "/urlshort-final", "url": "https://github.com/gophercises/urlshort/tree/solution"}]`
+	mux := getDefaultMux()
+	handler := getJSONHandler(mux)
 
 	rr := httptest.NewRecorder()
-	handler, err := JSONHandler([]byte(json), mux)
-	if err != nil {
-		panic(err)
-	}
-
 	handler.ServeHTTP(rr, req)
 
 	res := rr.Result()
