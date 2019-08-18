aee85da334c0cf852701b586c89a70db15bb4ffa
Pavel Savchenko <asfaltboy@gmail.com>
2019-08-18 15:02:22 +0200

    feat: use bold db in cmd

--- main/main.go
+++ main/main.go
@@ -9,28 +9,49 @@ import (
 	"os"
 
 	"github.com/asfaltboy/urlshort"
+	bolt "go.etcd.io/bbolt"
 )
 
 func main() {
-	path := flag.String("config", "example.yaml", "Path to yaml config file (see example.yaml)")
+	dbFile := flag.String("db", "example.db", "Path to bold db file (see https://godoc.org/go.etcd.io/bbolt)")
+	yaml := flag.String("yaml", "example.yaml", "Path to yaml config file (see example.yaml)")
+	json := flag.String("json", "", "Path to yaml config file (see example.yaml)")
 	flag.Parse()
-	file, err := os.Open(*path)
-	if err != nil {
-		log.Fatalf("could not open config file %s: %v", *path, err)
-	}
-	yaml, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatalf("could not parse config file: %v", err)
+
+	if *yaml == "" && *json == "" && *dbFile == "" {
+		log.Fatal("Must provide one source for path")
 	}
 
-	// Build the YAMLHandler using default 404 fallback mux
-	fallback := defaultMux()
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), fallback)
-	if err != nil {
-		log.Panicf("cannot build yaml handler: %v", err)
+	var mux http.Handler
+	var err error
+	mux = http.Handler(defaultMux())
+
+	if *yaml != "" {
+		mux, err = urlshort.YAMLHandler(readFileContent(*yaml), mux)
+		if err != nil {
+			log.Fatalf("cannot build yaml handler: %v", err)
+		}
 	}
+	if *json != "" {
+		mux, err = urlshort.JSONHandler(readFileContent(*json), mux)
+		if err != nil {
+			log.Fatalf("cannot build json handler: %v", err)
+		}
+	}
+	if *dbFile != "" {
+		db, err := bolt.Open(*dbFile, 0600, nil)
+		if err != nil {
+			log.Fatalf("error reading database file '%s': %v", *dbFile, err)
+		}
+		defer db.Close()
+		mux, err = urlshort.BoltHandler(db, mux)
+		if err != nil {
+			log.Fatalf("cannot build bolt handler: %v", err)
+		}
+	}
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mux)
 }
 
 func defaultMux() *http.ServeMux {
@@ -42,4 +63,16 @@ func defaultMux() *http.ServeMux {
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprintln(w, "Unknown urlshort entry!")
+}
+
+func readFileContent(path string) []byte {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("could not open config file %s: %v", path, err)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("could not read config file: %v", err)
+	}
+	return content
 }
